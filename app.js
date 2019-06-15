bfde5ad5a136643211ab4610eae9c66987cb3bf1
martin <martin.skara@hendur.eu>
2019-06-15 15:40:01 +0200

    test

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ const axios = require("axios");
 const app = express();
 const device = devices["Pixel 2 XL"];
 
-var port = process.env.PORT || 8080;
-
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
-app.get("/",(req, res, next) => {
-    res.json({});
-});
-
-app.get("/nodes1", (req, res, next) => {
+app.get("/nodes", (req, res, next) => {
     let searchstring = req.param("q");
     let url = 'https://www.google.com/search?q='+searchstring;
 
@@ -57,27 +51,6 @@ app.get("/nodes1", (req, res, next) => {
                 }
             }
             await processArray(kpp);
-            await browser.close();
-            await res.json(results);
-        } catch (err) {
-            console.log(error(err));
-            await browser.close();
-            console.log(error("Browser Closed"));
-        }
-        })();
-});
-
-app.get("/nodes2", (req, res, next) => {
-    let searchstring = req.param("q");
-    let url = 'https://www.google.com/search?q='+searchstring;
-
-    (async () => {
-        try {
-            let results = {"nodes":[]};
-            let browser = await puppeteer.launch({ headless: true });
-            let page = await browser.newPage();
-            await page.goto(url);
-            await page.waitFor(".knowledge-panel");
             let kpp_2 = await page.evaluate(() => Array.from( document.querySelectorAll('.knowledge-panel .kno-fb-ctx[data-rentity^="/"]'), element => element.getAttribute("data-rentity")));
             async function processArray2(array) {
                 for (const link of array) {
@@ -108,55 +81,6 @@ app.get("/nodes2", (req, res, next) => {
                 }
             }
             await processArray2(kpp_2);
-            await browser.close();
-            await res.json(results);
-        } catch (err) {
-            console.log(error(err));
-            await browser.close();
-            console.log(error("Browser Closed"));
-        }
-    })();
-});
-
-app.get("/nodes3", (req, res, next) => {
-    let searchstring = req.param("q");
-    let url = 'https://www.google.com/search?q='+searchstring;
-
-    (async () => {
-        try {
-            let results = {"nodes":[]};
-            let browser = await puppeteer.launch({ headless: true });
-            let page = await browser.newPage();
-            await page.goto(url);
-            await page.waitFor(".knowledge-panel");
-            async function processArray(array) {
-                for (const link of array) {
-                    console.log(link);
-                    await axios.get('https://kgsearch.googleapis.com/v1/entities:search', {
-                        params: {
-                            'query': link,
-                            'limit': 1,
-                            'indent': true,
-                            "key": "AIzaSyA94kim18rne3X5gzh7Gpl8Gt4SXz5yzuc",
-                            "languages": "cs"
-                        }
-                    }).then(function (response) {
-                        if (typeof response.data.itemListElement[0] !== 'undefined' && response.data.itemListElement[0]) {
-                            let item = response.data.itemListElement[0].result;
-                            if (item.name !== 'undefined' && item.name) {
-                                let graphitem = {
-                                    id: item["@id"],
-                                    label: item.name,
-                                    title: JSON.stringify(item, null, 4)
-                                };
-                                results["nodes"].push(graphitem);
-                            }
-                        }
-                    }).catch(function (error) {
-                        console.log(error);
-                    });
-                }
-            }
             let kpp_3 = await page.evaluate(() => Array.from(document.querySelectorAll('.knowledge-panel .kno-fb-ctx[data-rentity=""] > a'), element => element.getAttribute("title")));
             await processArray(kpp_3);
             await browser.close();
@@ -166,5 +90,5 @@ app.get("/nodes3", (req, res, next) => {
             await browser.close();
             console.log(error("Browser Closed"));
         }
-    })();
+        })();
 });
